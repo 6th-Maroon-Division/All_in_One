3859855173c87496e9f9decb9ab5107225d52f63
Alexander Grötzsch <a.g0201@gmx.de>
2025-08-26 20:05:23 +0200

    added 3 sound files and bumped version

---
+++ addons/sounds/config.cpp
@@ -0,0 +1,19 @@
+class CfgPatches
+{
+    class A6MD_All_in_One_sounds
+    {
+        requiredVersion = 1.00;
+        requiredAddons[] = {"A6MD_All_in_One_core"};
+    };
+};
+class CfgMusic
+{
+    #include "music.hpp"
+};
+class CfgMusicClasses
+{
+    class A6MD_Music
+    {
+        displayName = "6MD Music";
+    };
+};
